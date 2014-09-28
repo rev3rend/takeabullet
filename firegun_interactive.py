7974c73fdc2a46992d8e2d67b4935cd11f973856
rev3rend <rev3rend@gmail.com>
2014-09-27 23:59:53 -0400

    added real python stuff

--- firegun_interactive.py
+++ firegun_interactive.py
@@ -1,6 +1,3 @@
-# take a bullet for this city
-# stepper controller (non-interactive)
-
 import RPi.GPIO as GPIO
 import time, sys
 GPIO.setmode(GPIO.BCM)
@@ -15,6 +12,7 @@ GPIO.setup(coil_A_1_pin, GPIO.OUT)
 GPIO.setup(coil_A_2_pin, GPIO.OUT)
 GPIO.setup(coil_B_1_pin, GPIO.OUT)
 GPIO.setup(coil_B_2_pin, GPIO.OUT)
+
 
 def forward(delay, steps):
   for i in range(0, steps):
@@ -44,13 +42,30 @@ def setStep(w1, w2, w3, w4):
   GPIO.output(coil_B_1_pin, w3)
   GPIO.output(coil_B_2_pin, w4)
 
-def fire():
-  print("Python Firing Gun!!!")
+def main():
   steps = 10
   delay = 5/1000.0
   setStep(0, 0, 0, 0)
-  backward(delay,steps)
-  forward(delay,steps)
-  setStep(0, 0, 0, 0)
+  while True:
+    dir_in = raw_input("f/b/q:\n")
+    if dir_in.lower() == 'f':
+      forward(delay,steps)
+      setStep(0, 0, 0, 0)
+    elif dir_in.lower() == 'b':
+      backward(delay,steps)
+      setStep(0, 0, 0, 0)
+    elif dir_in.lower() == 's':
+      setStep(1, 0, 0, 1)
+      time.sleep(delay)
+      setStep(0, 1, 0, 1)
+    elif dir_in.lower() == 'l':
+      setStep(0, 0, 0, 0)
+    elif dir_in.lower() == 'a':
+      backward(delay,steps)
+      forward(delay,steps)
+      setStep(0, 0, 0, 0)
+    elif dir_in.lower() == 'q':
+      setStep(0, 0, 0, 0)
+      break
 
-fire()
+main()
